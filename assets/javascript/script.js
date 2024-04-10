a4f8374b23c94bcd23359e114ee9a08ebb3038bc
Caleb Saiia <115073504+AranosBanazir@users.noreply.github.com>
2024-04-10 16:49:12 -0500

    set up CDN, Imported CSS, and JS psuedo code.

---
+++ assets/javascript/script.js
@@ -0,0 +1,1 @@
+//Declaring elements to be used to get values from the form
---
+++ assets/javascript/trips.js
@@ -0,0 +1,2 @@
+//This will be to render the saved information from LS 
+//so that we can view previously scheduled trips by name, as well as the saved places of interest
